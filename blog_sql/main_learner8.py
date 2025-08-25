fcd26b64ee43706ca6c1b4f1fe24ba669ffab12a
Yogesh-rana-2301 <yogeshrana2301@gmail.com>
2025-08-25 20:17:34 +0530

    Add DB session handling and blog creation endpoint
    Introduces dependency injection for database sessions in FastAPI using get_db. Updates the /blog POST endpoint to create and persist a new blog entry in the database. Adds requirements.txt for project dependencies.

--- blog_sql/main_learner8.py
+++ blog_sql/main_learner8.py
@@ -6,20 +6,33 @@
 # the code is somewhat similar to the whole post code (but i would be storing the post queries in a database )
 #
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from typing import Optional
 # from pydantic import BaseModel #wont be using this because sending this to the schema file here
 import uvicorn   
 from . import schema
-from . import models
-from .database import engine
+from . import models 
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 
 app=FastAPI()
 
 models.base.metadata.create_all (bind=engine)
 
+def get_db():
+    db=SessionLocal()
+    try:
+        yield(db)  # yield is basically a return but dont terminates after excution 
+    finally:
+        db.close()
+
 @app.post ("/blog")
-async def create_blog(blog:schema.Blog):     
-    return {"data":blog}
+async def create_blog(blog:schema.Blog, db: Session=Depends(get_db)):    #doing this becasue Session is not a pydatic thing, so usign inbuilt from fastapi
+
+    new_blog=models.User(title=blog.title, body=blog.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
 
